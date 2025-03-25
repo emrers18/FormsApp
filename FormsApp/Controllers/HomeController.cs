042490b5757fcb28b8050f9b14aea05c79105ecb
Emre Ersin <emrersinn00@gmail.com>
2025-03-25 23:40:05 +0300

    Proje dosyası ekle.

---
+++ FormsApp/Controllers/HomeController.cs
@@ -0,0 +1,161 @@
+using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+
+namespace FormsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string searchString, string category)
+        {
+            var products = Repository.Products;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                ViewBag.SearchString = searchString;
+                products = products.Where(p => p.Name!.ToLower().Contains(searchString)).ToList();
+            }
+
+            if (!String.IsNullOrEmpty(category) && category != "0")
+            {
+                products = products.Where(p => p.CategoryId == int.Parse(category)).ToList();
+            }
+
+            //ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
+
+            var model = new ProductViewModel
+            {
+                Products = products,
+                Categories = Repository.Categories,
+                SelectedCategory = category
+            };
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product model, IFormFile imageFile)
+        {
+
+
+            var extension = "";
+            if (imageFile != null)
+            {
+
+                var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+                extension = Path.GetExtension(imageFile.FileName); // .jpg
+
+                if (!allowedExtensions.Contains(extension))
+                {
+                    ModelState.AddModelError("imageFile", "Lütfen .jpg, .jpeg veya .png uzantýlý dosya yükleyiniz.");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (imageFile != null)
+                {
+                    var randomFileName = string.Format($"{Guid.NewGuid()}{extension}"); // 123456789.jpg
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName); // FormsApp\wwwroot\img\123456789.jpg
+
+                    using (var stream = new FileStream(path, FileMode.Create)) // dosya yükleme iþlemi
+                    {
+                        await imageFile.CopyToAsync(stream);
+                    }
+
+                    model.Image = randomFileName;
+
+                    model.ProductId = Repository.Products.Count + 1;
+                    Repository.Products.Add(model);
+                    return RedirectToAction("Index");
+                }
+
+            }
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View(model);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View(entity);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Product model, IFormFile imageFile)
+        {
+
+            if (id != model.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (imageFile != null)
+                {
+
+                    var extension = Path.GetExtension(imageFile.FileName);
+                    var randomFileName = string.Format($"{Guid.NewGuid()}{extension}");
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await imageFile.CopyToAsync(stream);
+                    }
+                    model.Image = randomFileName;
+                }
+                Repository.EditProduct(model);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View(model);
+        }
+
+ 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            return View("DeleteConfirm", entity);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, int ProductId)
+        {
+            if (id != ProductId)
+            {
+                return NotFound();
+            }
+            var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            Repository.DeleteProduct(entity);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FormsApp/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
+{
+    public class Product
+    {
+        [Display(Name = "Urun Id")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage ="Lütfen Ürün Adını Giriniz.")]
+        [Display(Name = "Urun Adı")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Ürün Fiyatını Giriniz.")]
+        [Range(0,1000000)]
+        [Display(Name = "Fiyat")]
+        public decimal? Price { get; set; }
+
+        
+        [Display(Name = "Resim")]
+        public string? Image { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kategori Id Giriniz.")]
+        [Display(Name = "Kategori")]
+        public int? CategoryId { get; set; }
+    }
+}
